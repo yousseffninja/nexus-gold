9ef4a8a8671ef912d23a9812b4fa65ee0f6898d9
yousseffninja <yousseffmohamedd22@gmail.com>
2026-06-02 18:03:09 +0300

    Initial commit

---
+++ src/main/java/com/rocketeers/nexus_gold/NexusGoldApplication.java
@@ -0,0 +1,13 @@
+package com.rocketeers.nexus_gold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusGoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusGoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketeers/nexus_gold/NexusGoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketeers.nexus_gold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusGoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
