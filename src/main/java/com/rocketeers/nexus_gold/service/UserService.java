201b336136462bd90b63dcaa55f2cf02cc8e6c37
yousseffninja <yousseffmohamedd22@gmail.com>
2026-06-07 15:09:12 +0300

    feat: add change password endpoint

--- src/main/java/com/rocketeers/nexus_gold/service/UserService.java
+++ src/main/java/com/rocketeers/nexus_gold/service/UserService.java
@@ -1,7 +1,12 @@
 package com.rocketeers.nexus_gold.service;
 
+import com.rocketeers.nexus_gold.dto.ChangePasswordRequest;
+import com.rocketeers.nexus_gold.dto.ChangePasswordResponse;
+import com.rocketeers.nexus_gold.model.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService {
     UserDetailsService userDetailsService();
+
+    ChangePasswordResponse changePassword(User user, ChangePasswordRequest request);
 }
