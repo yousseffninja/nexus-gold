e0cb5f0d86b36e7a0790eec06f6175257d727872
yousseffninja <yousseffmohamedd22@gmail.com>
2026-06-21 08:44:05 +0300

    fix: fix forget password flow

--- src/main/java/com/rocketeers/nexus_gold/dto/ResetPasswordRequest.java
+++ src/main/java/com/rocketeers/nexus_gold/dto/ResetPasswordRequest.java
@@ -9,9 +9,6 @@ public class ResetPasswordRequest {
     @Schema(example = "abc123xyz", description ="Password reset token to identify the user")
     private String passwordResetToken;
 
-    @Schema(example = "12345", description = "code must be active ")
-    private String code;
-
     @Schema(example = "NewPassword123", description = "New password. Must be at least 8 characters")
     private String newPassword;
 }
---
+++ src/main/java/com/rocketeers/nexus_gold/dto/VerifyResetCodeRequest.java
@@ -0,0 +1,14 @@
+package com.rocketeers.nexus_gold.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class VerifyResetCodeRequest {
+
+    @Schema(example = "example@mail.com", description = "Email of the user")
+    private String email;
+
+    @Schema(example = "12345", description = "Reset code sent to email")
+    private String code;
+}
